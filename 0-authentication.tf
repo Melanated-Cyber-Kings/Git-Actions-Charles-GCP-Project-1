5052cdd4b4040d561bcea95acadc0e398051bb21
lynellg <lifeoflynell@gmail.com>
2025-08-05 21:02:29 -0400

    authentication terraform code

---
+++ 0-authentication.tf
@@ -0,0 +1,10 @@
+provider "google" {
+  project = "Jody65"
+  region  = "us-central1"
+  credentials = "jody65-b245cfcbfb74.json"
+
+}
+
+#Jody Engineer first authentication was here attempting to 
+
+#make changes to the region but will hold off untill we collectively decide
